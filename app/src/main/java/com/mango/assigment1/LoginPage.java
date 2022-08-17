1bba3dd3f77f9ff978ad869977c8113bd357b916
Smart <19011519-045@uog.edu.pk>
2022-08-17 15:47:15 +0500

    Form UI Updated

--- app/src/main/java/com/mango/assigment1/LoginPage.java
+++ app/src/main/java/com/mango/assigment1/LoginPage.java
@@ -2,6 +2,7 @@ package com.mango.assigment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginPage extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        findViewById(R.id.btnSign).setOnClickListener(v->{
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        findViewById(R.id.regText).setOnClickListener(v->{
+            Intent intent = new Intent(this,RegistrationPage.class);
+            startActivity(intent);
+        });
     }
 }
